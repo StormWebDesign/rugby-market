fdcd7c6330be3e0dd878ff74374b905e16b940d1
Russell English <russell.english@stormit.co.uk>
2025-03-12 17:33:33 +0000

    Added modal on Education and Work

--- src/components/dashboard-pages/candidates-dashboard/my-resume/components/Education.jsx
+++ src/components/dashboard-pages/candidates-dashboard/my-resume/components/Education.jsx
@@ -1,70 +1,156 @@
+import { useState, useEffect } from "react";
+import { db, auth } from "@/firebase";
+import { collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore";
+
 const Education = () => {
+  const [educationList, setEducationList] = useState([]);
+  const [newEducation, setNewEducation] = useState({
+    degree: "",
+    school: "",
+    startYear: "",
+    endYear: "",
+    description: "",
+  });
+
+  useEffect(() => {
+    const fetchEducation = async () => {
+      const user = auth.currentUser;
+      if (!user) return;
+
+      const querySnapshot = await getDocs(collection(db, `users/${user.uid}/education`));
+      const educationData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setEducationList(educationData);
+    };
+
+    fetchEducation();
+  }, []);
+
+  const addEducation = async (e) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user) {
+      alert("User not authenticated");
+      return;
+    }
+
+    try {
+      const docRef = await addDoc(collection(db, `users/${user.uid}/education`), newEducation);
+      setEducationList([...educationList, { id: docRef.id, ...newEducation }]);
+      setNewEducation({ degree: "", school: "", startYear: "", endYear: "", description: "" });
+
+      // Close the modal using Bootstrap's JS
+      window.bootstrap.Modal.getInstance(document.getElementById("educationModal")).hide();
+    } catch (error) {
+      console.error("Error adding education:", error);
+    }
+  };
+
   return (
     <div className="resume-outer">
       <div className="upper-title">
         <h4>Education</h4>
-        <button className="add-info-btn">
-          <span className="icon flaticon-plus"></span> Add Aducation
+        <button
+          className="add-info-btn"
+          data-bs-toggle="modal"
+          data-bs-target="#educationModal"
+        >
+          <span className="icon flaticon-plus"></span> Add Education
         </button>
       </div>
-      {/* <!-- Resume BLock --> */}
-      <div className="resume-block">
-        <div className="inner">
-          <span className="name">M</span>
-          <div className="title-box">
-            <div className="info-box">
-              <h3>Bachlors in Fine Arts</h3>
-              <span>Modern College</span>
-            </div>
-            <div className="edit-box">
-              <span className="year">2012 - 2014</span>
-              <div className="edit-btns">
-                <button>
-                  <span className="la la-pencil"></span>
-                </button>
-                <button>
-                  <span className="la la-trash"></span>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
-            ipsum tellus. Interdum et malesuada fames ac ante
-            <br /> ipsum primis in faucibus.
-          </div>
-        </div>
-      </div>
 
-      {/* <!-- Resume BLock --> */}
-      <div className="resume-block">
-        <div className="inner">
-          <span className="name">H</span>
-          <div className="title-box">
-            <div className="info-box">
-              <h3>Computer Science</h3>
-              <span>Harvard University</span>
-            </div>
-            <div className="edit-box">
-              <span className="year">2008 - 2012</span>
-              <div className="edit-btns">
-                <button>
-                  <span className="la la-pencil"></span>
-                </button>
-                <button>
-                  <span className="la la-trash"></span>
-                </button>
+      {/* Bootstrap Modal */}
+      <div className="modal fade" id="educationModal">
+        <div className="modal-dialog modal-lg modal-dialog-centered">
+          <div className="modal-content">
+            <button type="button" className="closed-modal" data-bs-dismiss="modal"></button>
+            <div className="modal-body">
+              <h3>Add Education</h3>
+              <form onSubmit={addEducation}>
+                <div className="row">
+                  <div className="form-group col-lg-6 col-md-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Degree"
+                      value={newEducation.degree}
+                      onChange={(e) => setNewEducation({ ...newEducation, degree: e.target.value })}
+                      required
+                    />
+                  </div>
+                  <div className="form-group col-lg-6 col-md-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="School"
+                      value={newEducation.school}
+                      onChange={(e) => setNewEducation({ ...newEducation, school: e.target.value })}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group col-lg-6 col-md-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Start Year"
+                      value={newEducation.startYear}
+                      onChange={(e) => setNewEducation({ ...newEducation, startYear: e.target.value })}
+                      required
+                    />
+                  </div>
+                  <div className="form-group col-lg-6 col-md-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="End Year"
+                      value={newEducation.endYear}
+                      onChange={(e) => setNewEducation({ ...newEducation, endYear: e.target.value })}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group col-md-12">
+                <textarea
+                  className="form-control"
+                  placeholder="Description"
+                  value={newEducation.description}
+                  onChange={(e) => setNewEducation({ ...newEducation, description: e.target.value })}
+                ></textarea>
+                </div>
               </div>
+                <button type="submit" className="theme-btn btn-style-one">
+                  Save
+                </button>
+              </form>
             </div>
-          </div>
-          <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
-            ipsum tellus. Interdum et malesuada fames ac ante
-            <br /> ipsum primis in faucibus.
           </div>
         </div>
-      </div>
-    </div>
+      </div >
+
+      {
+        educationList.map((edu) => (
+          <div className="resume-block" key={edu.id}>
+            <div className="inner">
+              <span className="name">{edu.degree.charAt(0)}</span>
+              <div className="title-box">
+                <div className="info-box">
+                  <h3>{edu.degree}</h3>
+                  <span>{edu.school}</span>
+                </div>
+                <div className="edit-box">
+                  <span className="year">{edu.startYear} - {edu.endYear}</span>
+                  <button onClick={() => deleteDoc(doc(db, `users/${auth.currentUser.uid}/education`, edu.id))}>
+                    <span className="la la-trash"></span>
+                  </button>
+                </div>
+              </div>
+              <div className="text">{edu.description}</div>
+            </div>
+          </div>
+        ))
+      }
+    </div >
   );
 };
 
--- src/components/dashboard-pages/candidates-dashboard/my-resume/components/Experiences.jsx
+++ src/components/dashboard-pages/candidates-dashboard/my-resume/components/Experiences.jsx
@@ -1,66 +1,130 @@
+import { useState, useEffect } from "react";
+import { db, auth } from "@/firebase";
+import { collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore";
+
 const Experiences = () => {
+  const [workList, setWorkList] = useState([]);
+  const [newWork, setNewWork] = useState({
+    title: "",
+    company: "",
+    startYear: "",
+    endYear: "",
+    description: "",
+  });
+
+  useEffect(() => {
+    const fetchWork = async () => {
+      const user = auth.currentUser;
+      if (!user) return;
+
+      const querySnapshot = await getDocs(collection(db, `users/${user.uid}/work_experience`));
+      const workData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setWorkList(workData);
+    };
+
+    fetchWork();
+  }, []);
+
+  const addWork = async (e) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user) {
+      alert("User not authenticated");
+      return;
+    }
+
+    try {
+      const docRef = await addDoc(collection(db, `users/${user.uid}/work_experience`), newWork);
+      setWorkList([...workList, { id: docRef.id, ...newWork }]);
+      setNewWork({ title: "", company: "", startYear: "", endYear: "", description: "" });
+
+      // Close the modal using Bootstrap's JS
+      window.bootstrap.Modal.getInstance(document.getElementById("workModal")).hide();
+    } catch (error) {
+      console.error("Error adding work experience:", error);
+    }
+  };
+
   return (
     <div className="resume-outer theme-blue">
       <div className="upper-title">
         <h4>Work & Experience</h4>
-        <button className="add-info-btn">
+        <button
+          className="add-info-btn"
+          data-bs-toggle="modal"
+          data-bs-target="#workModal"
+        >
           <span className="icon flaticon-plus"></span> Add Work
         </button>
       </div>
-      {/* <!-- Resume BLock --> */}
-      <div className="resume-block">
-        <div className="inner">
-          <span className="name">S</span>
-          <div className="title-box">
-            <div className="info-box">
-              <h3>Product Designer</h3>
-              <span>Spotify Inc.</span>
-            </div>
-            <div className="edit-box">
-              <span className="year">2012 - 2014</span>
-              <div className="edit-btns">
-                <button>
-                  <span className="la la-pencil"></span>
-                </button>
-                <button>
-                  <span className="la la-trash"></span>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
-            ipsum tellus. Interdum et malesuada fames ac ante
-            <br /> ipsum primis in faucibus.
-          </div>
-        </div>
-      </div>
 
-      {/* <!-- Resume BLock --> */}
-      <div className="resume-block">
-        <div className="inner">
-          <span className="name">D</span>
-          <div className="title-box">
-            <div className="info-box">
-              <h3>Sr UX Engineer</h3>
-              <span>Dropbox Inc.</span>
-            </div>
-            <div className="edit-box">
-              <span className="year">2012 - 2014</span>
-              <div className="edit-btns">
-                <button>
-                  <span className="la la-pencil"></span>
+
+      <div className="modal fade" id="workModal">
+        <div className="modal-dialog modal-lg modal-dialog-centered">
+          <div className="modal-content">
+            <button type="button" className="closed-modal" data-bs-dismiss="modal"></button>
+            <div className="modal-body">
+              <h3>Add Work Experience</h3>
+              <form onSubmit={addWork}>
+                <div className="row">
+                  <div className="form-group col-lg-6 col-md-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Job Title"
+                      value={newWork.title}
+                      onChange={(e) => setNewWork({ ...newWork, title: e.target.value })}
+                      required
+                    />
+                  </div>
+                  <div className="form-group col-lg-6 col-md-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Company"
+                      value={newWork.company}
+                      onChange={(e) => setNewWork({ ...newWork, company: e.target.value })}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group col-lg-6 col-md-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Start Year"
+                      value={newWork.startYear}
+                      onChange={(e) => setNewWork({ ...newWork, startYear: e.target.value })}
+                      required
+                    />
+                  </div>
+                  <div className="form-group col-lg-6 col-md-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="End Year"
+                      value={newWork.endYear}
+                      onChange={(e) => setNewWork({ ...newWork, endYear: e.target.value })}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group col-md-12">
+                    <textarea
+                      className="form-control"
+                      placeholder="Enter some details about the job"
+                      value={newWork.description}
+                      onChange={(e) => setNewWork({ ...newWork, description: e.target.value })}
+                    ></textarea>
+                  </div>
+                </div>
+                <button type="submit" className="theme-btn btn-style-one">
+                  Save
                 </button>
-                <button>
-                  <span className="la la-trash"></span>
-                </button>
-              </div>
+              </form>
             </div>
-          </div>
-          <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
-            ipsum tellus. Interdum et malesuada fames ac ante
-            <br /> ipsum primis in faucibus.
           </div>
         </div>
       </div>
